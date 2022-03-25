8d52c5779ea8bd3752213eb58c16d83dd30059b1
George <Linkeydoo@gmail.com>
2022-03-25 16:34:54 -0400

    Update FermatPrimality.py

--- FermatPrimality.py
+++ FermatPrimality.py
@@ -1,28 +1,34 @@
+import math
+import random
+from threading import Thread
+
 def main():
-    print("hello")
+    carmichaelCound = 0
+    for i in range((10**6), 3, -1):
+        if carmichaelCound == 3:
+            break
+        new_thread = Thread(target=checkCar(i))
+        new_thread.start()
 
-def squareMult(base, exponent, mod):
-    binaryExp = format(exponent, 'b')
-    print(f'In Binary:{binaryExp}')
+def checkCar(i):
+    if fermat(i, 4):
+        if not checkPrime(i):
+            print(f"{i}: Is a Carmichael Number")
+# retuens False on composite and true on prime
+def fermat(p, s):
+    for i in range(1, s):
+        a = random.randint(2, p-2)
+        if ((a**(p-1)) % p) != 1:
+            return False
+    return True
 
-    square = 1
-    mult = 0
-    print(f'Length of item: {len(binaryExp)}')
-    for i in range(len(binaryExp)):
-        # print(f'The {i}th Element is: {binaryExp[i]}')
-        print(f'Round: {i+1}')
-        square = (square ** 2) % mod
-        print(f'square: {square}')
-        if binaryExp[i] == '1':
-            mult = (square * base) % mod
-            square = mult
-            print(f'mult: {mult}')
-        else:
-            print("mult: -")
-        print('~~~~~~~~~~~~~~~~~')
-    print(f'Final Answer: {square}')
-
-
+# Returns True on Prime and False on Composite
+def checkPrime(num):
+    stoppingPoint = int(num ** 0.5)
+    for i in range(2, stoppingPoint+1):
+        if (num % i == 0):
+            return False
+    return True
 if __name__ == "__main__":
     main()
 
