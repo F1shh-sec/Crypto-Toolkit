2b720ca926277d9abe8cbc7f0d4ed796d85ca3f6
George <Linkeydoo@gmail.com>
2022-04-01 21:00:28 -0400

    Update generatorfinder.go

--- generatorfinder.go
+++ generatorfinder.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/big"
 	"strconv"
@@ -8,9 +9,16 @@ import (
 )
 
 var wg sync.WaitGroup
+var pow int
+
+func init() {
+	power := flag.Int("p", 1, "mod")
+	flag.Parse()
+	pow = *power
+}
 
 func main() {
-	for x := 1000; x < 4968; x++ {
+	for x := 1; x < pow; x++ {
 		wg.Add(1)
 		// Tests the exponent x
 		go test_primitive(x)
@@ -25,11 +33,11 @@ func test_primitive(input int) {
 	numToTest := int64(input)
 	result := big.Int{}
 	firstNum := big.NewInt(numToTest)
-	secondNum := big.NewInt(4968)
+	secondNum := big.NewInt(int64(pow))
 	gcd := result.GCD(nil, nil, firstNum, secondNum)
 	intGCD := gcd.Int64()
 	if int(intGCD) == 1 {
-		fmt.Println(strconv.Itoa(input) + " Is primitive WITH GCD: " + strconv.Itoa(int(intGCD)))
+		fmt.Print(strconv.Itoa(input) + ", ")
 	} else {
 		//fmt.Println(strconv.Itoa(input) + " Is Not Primitive")
 	}
